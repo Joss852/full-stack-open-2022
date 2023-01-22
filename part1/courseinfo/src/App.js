d324fc08650cf97cf2f3f63f0b5db8f54172424a
Josseph Morales <johnymoore852@gmail.com>
2023-01-22 16:41:31 -0600

    1.11: unicafe step6

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,33 +1,51 @@
 import { useState } from 'react'
 
 const Button = ({ handleClick, text }) => {
-  return <button onClick={handleClick}>{text}</button>
+  return (
+    <button type='button' onClick={handleClick}>
+      {text}
+    </button>
+  )
 }
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
+    <td>
       {text} {value}
-    </p>
+    </td>
   )
 }
 
 const Statistics = ({ good, neutral, bad }) => {
   return (
-    <div>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={good + neutral + bad} />
-      <StatisticLine
-        text='average'
-        value={(good - bad) / (good + neutral + bad)}
-      />
-      <StatisticLine
-        text='positive'
-        value={(good * 100) / (good + neutral + bad) + ' %'}
-      />
-    </div>
+    <table>
+      <tbody>
+        <tr>
+          <StatisticLine text='good' value={good} />
+        </tr>
+        <tr>
+          <StatisticLine text='neutral' value={neutral} />
+        </tr>
+        <tr>
+          <StatisticLine text='bad' value={bad} />
+        </tr>
+        <tr>
+          <StatisticLine text='all' value={good + neutral + bad} />
+        </tr>
+        <tr>
+          <StatisticLine
+            text='average'
+            value={(good - bad) / (good + neutral + bad)}
+          />
+        </tr>
+        <tr>
+          <StatisticLine
+            text='positive'
+            value={(good * 100) / (good + neutral + bad) + ' %'}
+          />
+        </tr>
+      </tbody>
+    </table>
   )
 }
 

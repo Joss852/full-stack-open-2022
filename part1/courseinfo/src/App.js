e25a16731b035beadb7236c8c24a80877aa02e65
Josseph Morales <johnymoore852@gmail.com>
2023-01-22 16:10:21 -0600

    1.6: unicafe step1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,77 +1,22 @@
+import { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      },
-    ],
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  return (
-    <>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </>
-  )
-}
-
-const Header = (props) => (
-  <header>
-    <h1>{props.course.name}</h1>
-  </header>
-)
-
-const Content = (props) => {
   return (
     <div>
-      <Part
-        part={props.course.parts[0].name}
-        exercises={props.course.parts[0].exercises}
-      />
-      <Part
-        part={props.course.parts[1].name}
-        exercises={props.course.parts[1].exercises}
-      />
-      <Part
-        part={props.course.parts[2].name}
-        exercises={props.course.parts[2].exercises}
-      />
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <footer>
-      <p>
-        Number of exercises {
-          props.course.parts[0].exercises +
-          props.course.parts[1].exercises +
-          props.course.parts[2].exercises
-        }
-      </p>
-    </footer>
   )
 }
 

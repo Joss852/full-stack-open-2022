81e4c628551bef3e9c892a75a53b7367c789d539
Josseph Morales <johnymoore852@gmail.com>
2023-01-22 16:19:44 -0600

    1.9: unicafe step4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,12 +4,18 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {(good * 100) / (good + neutral + bad)} %</p>
+      {good || neutral || bad ? (
+        <div>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {good + neutral + bad}</p>
+          <p>average {(good - bad) / (good + neutral + bad)}</p>
+          <p>positive {(good * 100) / (good + neutral + bad)} %</p>
+        </div>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   )
 }

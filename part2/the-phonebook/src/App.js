6d2e4e71dc96bb19d32d236ea401e754bca774e0
Josseph Morales <johnymoore852@gmail.com>
2023-01-22 20:34:38 -0600

    2.8: The Phonebook Step3

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,18 +1,23 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '040-1234567' },
+  ])
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const handleSubmit = e => {
     e.preventDefault()
     const person = {
       name: newName,
+      phone: newPhone,
     }
-    
+
     if (!persons.find(person => person.name === newName)) {
       setPersons(persons.concat(person))
       setNewName('')
+      setNewPhone('')
       return
     }
 
@@ -28,12 +33,19 @@ const App = () => {
           <input value={newName} onChange={e => setNewName(e.target.value)} />
         </div>
         <div>
+          phone:{' '}
+          <input value={newPhone} onChange={e => setNewPhone(e.target.value)} />
+        </div>
+        <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map(person => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name}
+          {person.phone}
+        </p>
       ))}
       <div>debug: {newName}</div>
     </div>
